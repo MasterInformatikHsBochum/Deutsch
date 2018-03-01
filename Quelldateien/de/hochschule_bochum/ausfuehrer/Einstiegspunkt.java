e69a5011932e3995296316479e410309cee7b7b9
Marius Meisenzahl <mariusmeisenzahl@gmail.com>
2018-03-01 07:47:55 +0100

    Kompilator und Ausführer hinzugefügt

---
+++ Quelldateien/de/hochschule_bochum/ausfuehrer/Einstiegspunkt.java
@@ -0,0 +1,9 @@
+package de.hochschule_bochum.ausfuehrer;
+
+public class Einstiegspunkt {
+    public static int main(String args[]) {
+        System.out.println("Ausführer - Deutsch");
+
+        return 0;
+    }
+}
---
+++ Quelldateien/de/hochschule_bochum/kompilator/Einstiegspunkt.java
@@ -0,0 +1,9 @@
+package de.hochschule_bochum.kompilator;
+
+public class Einstiegspunkt {
+    public static int main(String args[]) {
+        System.out.println("Kompilator - Deutsch");
+
+        return 0;
+    }
+}
